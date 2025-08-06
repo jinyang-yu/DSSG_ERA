9fe481fb534b7aa8e21530beb738c8ee2ba6913e
jinyang-yu <yujy959@gmail.com>
2025-08-06 10:38:20 -0700

    update main

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 
 from web_scraper.run_scraper import run_scraper
 from classifier.run_classifier import run_classifier
+<<<<<<< HEAD
 import asyncio
 # from risk_analysis import risk_analysis_main
+=======
+from risk_analysis.run_risk_analysis import pdfs_risk_analysis
+from dotenv import load_dotenv
+from openai import OpenAI
+>>>>>>> 86fed49 (update main)
 
 async def web_main():
   """
@@ -22,8 +28,23 @@ def pdf_main():
   """
   Main entry point to initialize and run pdf pipeline
   """
+<<<<<<< HEAD
   print("Beginning PDF risk extraction...")
   # risk_analysis_main
+=======
+    # Load environment variables from .env file
+    load_dotenv()
+
+    ### creates an instance of the OpenAI client
+    client = OpenAI()
+
+    ### importing developer instructions
+    instructions_path = 'risk_analysis/instructions.txt'
+    with open(instructions_path, 'r', encoding='utf-8') as f:
+        dev_instructions = f.read()
+
+    pdfs_risk_analysis()
+>>>>>>> 86fed49 (update main)
   
 if __name__ == "__main__":
     asyncio.run(web_main())
